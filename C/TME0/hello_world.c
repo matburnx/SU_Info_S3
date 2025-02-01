79b54d64f8eb1d6fe4c4d8b5275a887005dd2f11
Matéo Boury <matboury@proton.me>
2025-02-01 20:06:54 +0000

    Delete C directory

--- C/TME0/hello_world.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Bonjour monde !\n");
-    return 0;
-}
--- C/TME5/devel.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DEVEL_H_
-#define _DEVEL_H_
-
-#include<stdio.h>
-
-#define affiche_message(x) printf("%s, ligne %d, fonction %s\n", (x), __LINE__, __PRETTY_FUNCTION__)
-
-#endif
